6fc6d971c14ed84cf61a26d15fba6493710b1228
Gabriel Duarte <gdurte.plus@gmail.com>
2025-07-02 11:13:36 -0300

    ajustes de portaria

--- resources/js/pages/auth/login2.tsx
+++ resources/js/pages/auth/login2.tsx
@@ -194,7 +194,7 @@ export default function Login({ status, canResetPassword}: LoginProps) {
                                 </svg>
                                 {isLoading ? 'Conectando...' : 'Entrar com Google'}
                             </Button>
-                            v2.0.1
+                            v2.0.2
                         </div>
                     </CardContent>
                 </Card>
